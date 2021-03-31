9c0a05925dca34a835497912e76c17e255f49392
Jaysen Spurlock <jayspur11@gmail.com>
2021-03-30 22:00:56 -0700

    Rework gateway

--- restapi/gateway.py
+++ restapi/gateway.py
@@ -2,8 +2,12 @@ from urllib.request import Request
 from restapi import _core
 
 
+def _gateway_endpoint(path):
+    return _core.BASE_API_URL + "/gateway/" + path
+
+
 # Public methods
 def get_bot():
-    url = _core.BASE_API_URL + "/gateway/bot"
+    url = _gateway_endpoint("bot")
     request = Request(url)
     return _core.make_request(request)

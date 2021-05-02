e7d2a46facd7f40974934cf2b1e47fad1d793814
Jaysen Spurlock <jayspur11@gmail.com>
2021-05-02 13:01:18 -0700

    Reuse common endpoint -> request code

--- restapi/_core.py
+++ restapi/_core.py
@@ -1,8 +1,14 @@
 import json
+from restapi import url
 from urllib.request import Request, urlopen
 
 app_name = None
 bot_token = None
+
+
+def make_get_request(endpoint: url.URL):
+    request = Request(endpoint.build())
+    return make_request(request)
 
 
 def make_request(request: Request):
--- restapi/channel.py
+++ restapi/channel.py
@@ -6,8 +6,7 @@ from restapi import url
 
 def get_channel(channel_id):
     endpoint = url.URL("/channels/{cid}".format(cid=channel_id))
-    request = Request(endpoint.build())
-    return _core.make_request(request)
+    return _core.make_get_request(endpoint)
 
 
 def get_messages(channel_id,
@@ -23,15 +22,13 @@ def get_messages(channel_id,
         "after": after_message
     })
 
-    request = Request(endpoint.build())
-    return _core.make_request(request)
+    return _core.make_get_request(endpoint)
 
 
 def get_message(channel_id, message_id):
     endpoint = url.URL("/channels/{cid}/messages/{mid}".format(cid=channel_id,
                                                                mid=message_id))
-    request = Request(endpoint.build())
-    return _core.make_request(request)
+    return _core.make_get_request(endpoint)
 
 
 def get_reactions(channel_id, message_id, emoji, after=None, limit=None):
@@ -42,17 +39,14 @@ def get_reactions(channel_id, message_id, emoji, after=None, limit=None):
         "after": after,
         "limit": str(limit)
     })
-    request = Request(endpoint.build())
-    return _core.make_request(request)
+    return _core.make_get_request(endpoint)
 
 
 def get_invites(channel_id):
     endpoint = url.URL(f"/channels/{channel_id}/invites")
-    request = Request(endpoint.build())
-    return _core.make_request(request)
+    return _core.make_get_request(endpoint)
 
 
 def get_pinned_messages(channel_id):
     endpoint = url.URL(f"/channels/{channel_id}/pins")
-    request = Request(endpoint.build())
-    return _core.make_request(request)
+    return _core.make_get_request(endpoint)

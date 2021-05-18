2972321f713d5c21cd3a0b1d577100f35d4c28c8
Jaysen Spurlock <jayspur11@gmail.com>
2021-05-17 21:30:39 -0700

    Alias snowflake

--- objects/snowflake.py
+++ objects/snowflake.py
@@ -1,9 +1,12 @@
 _DISCORD_EPOCH = 1420070400000  # first second of 2015
 
 
-def snowflake_to_timestamp(snowflake: str) -> int:
+Snowflake = str
+
+
+def snowflake_to_timestamp(snowflake: Snowflake) -> int:
     return (int(snowflake) >> 22) + _DISCORD_EPOCH
 
 
-def timestamp_to_snowflake(timestamp: int) -> str:
-    return str((timestamp - _DISCORD_EPOCH) << 22)
+def timestamp_to_snowflake(timestamp: int) -> Snowflake:
+    return Snowflake((timestamp - _DISCORD_EPOCH) << 22)
